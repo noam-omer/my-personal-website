9a93619dc9dae0c0a26c21f9566a32b0b5e7aab9
Noam Fischhoff <nfischhoff@gmail.com>
2022-07-29 13:05:44 -0400

    invasive species page

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+/*
 const h2 = document.createElement('h2');
 h2.textContent = 'This content added by Javascript';
-//document.querySelector('body').appendChild(h2);
+document.querySelector('body').appendChild(h2);
+*/
+const invasiveButton = document.getElementById('submitButton');
+invasiveButton.addEventListener('click', function() {
+  if(document.getElementById('knotweed').checked)
+    alert('Good choice!');
+  else if (document.getElementById('treeOfHeaven').checked)
+    alert('Not as common in Pittsburgh, but also bad');
+  else if (document.getElementById('spottedLanternfly').checked)
+    alert('A brand new problem!');
+  else alert('Please pick a species');
+});
+
+
